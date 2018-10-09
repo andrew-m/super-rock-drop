c8b83f117fecac762b3d6fef0d9d74598d2db6f8
andrew-m <andrew-m@users.noreply.github.com>
2018-10-09 08:03:11 +0100

    First mocha/chai test stub runs with 'npm test'

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+function blah () {
+    var canvas = doc.getElementById("canvas");
+    width = canvas.width;
+    height = canvas.height;
+    sizeOfGridSquareX = width / gridWidth;
+    sizeOfGridSquareY = height / gridHeight; //un hard code these.
+    ctx = canvas.getContext("2d");
+    console.log("Hellow");
+}
---
+++ test/firstSpec.js
@@ -0,0 +1,19 @@
+require('mocha')
+const { expect } = require('chai')
+var assert = require('assert');
+
+describe('Babylon user id rule', function() {
+
+    it('should copy the user ids from the user object to the context object', function() {
+            expect(true).to.equal(true)
+    })
+})
+
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});
