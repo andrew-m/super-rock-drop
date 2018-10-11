e99733f0e3da5cb9f02541a7555f31fc8816bcb7
andrew-m <andrew-m@users.noreply.github.com>
2018-10-11 09:15:59 +0100

    Javascript working, canvas drawing. Next needs tests and composition

--- renderer.js
+++ renderer.js
@@ -2,12 +2,46 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-function blah () {
+const blah = function (doc) {
+    if (doc === null || doc === undefined) {
+        console.log("doc is null :(");
+        return;
+    }
     var canvas = doc.getElementById("canvas");
     width = canvas.width;
     height = canvas.height;
-    sizeOfGridSquareX = width / gridWidth;
-    sizeOfGridSquareY = height / gridHeight; //un hard code these.
+    sizeOfGridSquareX = width / 6;
+    sizeOfGridSquareY = height / 12; //ToDo: un hard code these.
     ctx = canvas.getContext("2d");
     console.log("Hellow");
+
+    ctx.fillStyle = "#ffffff";
+
+    // var centerX = (entity.gameGridPosition.x * sizeOfGridSquareX) - (sizeOfGridSquareX / 2);
+    // var centery = (entity.gameGridPosition.y * sizeOfGridSquareY) - (sizeOfGridSquareY / 2);
+
+    ctx.fillRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    ctx.fillStyle = "red";
+
+    // var centerX = (entity.gameGridPosition.x * sizeOfGridSquareX) - (sizeOfGridSquareX / 2);
+    // var centery = (entity.gameGridPosition.y * sizeOfGridSquareY) - (sizeOfGridSquareY / 2);
+
+    ctx.fillRect(
+        50,
+        50,
+        20,
+        20
+    );
+
+
+}
+
+module.exports = {
+    blah
 }
--- test/firstSpec.js
+++ test/firstSpec.js
@@ -9,7 +9,6 @@ describe('Babylon user id rule', function() {
     })
 })
 
-
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
