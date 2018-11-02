bd5f7bc094ac8a0401826b9b23463e6159b3c4e1
andrew-m <andrew-m@users.noreply.github.com>
2018-11-02 18:27:39 +0000

    Rename blah to setup. Clean slate for turning attention back to tests.

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const blah = function (doc) {
+const setup = function (doc) {
     if (doc === null || doc === undefined) {
         console.log("doc is null :(");
         return;
@@ -43,5 +43,5 @@ const blah = function (doc) {
 }
 
 module.exports = {
-    blah
+    setup
 }
