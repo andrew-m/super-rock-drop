3eae5e3dade2b8fa27beb21523c0697fce69c474
andrew-m <andrew-m@users.noreply.github.com>
2019-07-31 08:55:34 +0100

    Minor tweak to window size, now shows whole game by default.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, backgroundColor: '#90bf90'})
+  mainWindow = new BrowserWindow({width: 600, height: 800, backgroundColor: '#90bf90'})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
